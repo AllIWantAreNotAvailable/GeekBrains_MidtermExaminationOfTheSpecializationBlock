dc838ce94536bc957acf54249ad9ed743b128fc4
Momotov Kirill <unknownman@live.ru>
2023-07-21 11:47:18 +0300

    View was finished
    On branch python-Drafts-alpha2
    
    Changes to be committed:
    	modified:   Python-Drafts/application/view.py

--- Python-Drafts/application/view.py
+++ Python-Drafts/application/view.py
@@ -1,12 +1,36 @@
 from abc import ABC, abstractmethod
 
 
-class BaseView(ABC):
+class View(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def output(text_out: str):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def input(invitation: str):
+        pass
+
+
+class MainView(View):
 
     def __init__(self, controller):
         self.controller = controller
 
-    @abstractmethod
+    @property
     def controller(self):
-        pass
+        return self.__controller
 
+    @controller.setter
+    def controller(self, controller):
+        self.__controller = controller
+
+    @staticmethod
+    def output(text_out: str):
+        print(text_out)
+
+    @staticmethod
+    def input(invitation: str):
+        return input('\n'.join([invitation, '>>> ']))
